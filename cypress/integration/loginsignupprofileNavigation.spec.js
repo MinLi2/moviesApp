bac1a74484683ff6bf876ae6e5181fc9713beb9e
Min Li <20091580@mail.wit.ie>
2020-12-14 08:29:38 +0800

    E2E tests- login signup and dashboard

--- cypress/integration/loginsignupprofileNavigation.spec.js
+++ cypress/integration/loginsignupprofileNavigation.spec.js
@@ -3,22 +3,27 @@ let password;
 describe("From the home page", () => {
     beforeEach(() => {
       cy.visit("/");
-      cy.get("nav").find("li").eq(6).click();
+     
     });
-    it("should display the login page and change browser URL", () => {
-      
-        cy.get('input').eq(0).type('892397497@qq.com');
-        cy.get('input').eq(1).type('111111');
-        cy.get('button').click();
-        cy.url().should("include", `/Dashboard`);
-      
-    });
+  
     it("should navigate to the sign up page and change browser URL", () => {
        
-       
+        cy.get("nav").find("li").eq(6).click();
        // cy.get('a[href*="/login"]').contains("Login").click();
         cy.get('a').contains("Sign Up").click();
         cy.url().should("include", `/signup`);
       
     });
+    
+       
+        it("should navigate to the login page and change browser URL", () => {
+            cy.get("nav").find("li").eq(7).click();
+           
+            cy.get('a[href*="/login"]').contains("Login").click();
+            cy.url().should("include", `/login`);
+          
+        });
+  
+      
+   
 })
--- cypress/integration/signup-page.spec.js
+++ cypress/integration/signup-page.spec.js
@@ -13,8 +13,8 @@ describe("From the home page", () => {
     //     cy.url().should("include", `/Dashboard`);
       
     // });
-    it("should navigate to the sign up page and change browser URL", () => {
-        cy.get("nav").find("li").eq(6).click();
+    it("should navigate to the login page and change browser URL", () => {
+        cy.get("nav").find("li").eq(7).click();
        
         cy.get('a[href*="/login"]').contains("Login").click();
         cy.url().should("include", `/login`);
