af63d4ca5bb46b0ab0f5b56713df63cb23791456
Min Li <20091580@mail.wit.ie>
2020-12-16 20:59:44 +0800

    code splitting

--- src/index.js
+++ src/index.js
@@ -1,32 +1,32 @@
-import React from "react";
+import React, { lazy, Suspense  } from "react";
 import ReactDOM from "react-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
-import HomePage from "./pages/homePage";
-import MoviePage from './pages/movieDetailsPage';
-import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom"    // CHANGED
-import FavoriteMoviesPage from './pages/favoritesMoviesPage'       // NEW
-import MovieReviewPage from "./pages/movieReviewPage";
-import SiteHeader from './components/siteHeader'
-import UpcomingMoviePage from "./pages/UpcomingMoviePage";
-import NowPlayingMoviePage from "./pages/NowPlayingMoviePage";
-import FavoritePeoplePage from "./pages/favoritePeoplePage";
-import PeoplePage from "./pages/peopleDetailsPage";
-import MoviePeoplePage from "./pages/moviePeoplePage";
-import RateMoviePage from "./pages/rateMoviePage";
-import LoginPage from"./pages/loginPage";
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom" 
 import MoviesContextProvider from "./contexts/moviesContext";
 import GenresContextProvider from "./contexts/genresContext";
 import PeopleContextProvider from "./contexts/peopleContext";
-import AddMovieReviewPage from './pages/addMovieReviewPage';
-import AddPeopleReviewPage from './pages/addPeopleReviewPage';
-import { AuthProvider } from "./contexts/AuthContext";
-import SignUpPage from "./pages/signUpPage";
-import DashboardPage from "./pages/DashboardPage";
-import PrivateRoute from "./components/PrivateRoute";
-import ForgotPasswordPage from "./pages/ForgotPasswordPage";
-import UpdateProfilePage from "./pages/UpdateProfilePage";
-import TopRatedMovie from "./pages/topRatedMovie";
-import SimilarMoviesPage from "./pages/similarMoviePage";
+import { AuthProvider } from "./contexts/AuthContext";   // CHANGED
+import SiteHeader from './components/siteHeader';
+const UpcomingMoviePage = lazy(() => import("./pages/UpcomingMoviePage"));
+const NowPlayingMoviePage = lazy(() => import("./pages/NowPlayingMoviePage"));
+const FavoritePeoplePage = lazy(() => import("./pages/favoritePeoplePage"));
+const PeoplePage = lazy(() => import("./pages/peopleDetailsPage"));
+const MoviePeoplePage = lazy(() => import("./pages/moviePeoplePage"));
+const RateMoviePage = lazy(() => import("./pages/rateMoviePage"));
+const LoginPage = lazy(() => import("./pages/loginPage"));
+const AddMovieReviewPage = lazy(() => import('./pages/addMovieReviewPage'));
+const AddPeopleReviewPage = lazy(() => import('./pages/addPeopleReviewPage'));
+const SignUpPage = lazy(() => import("./pages/signUpPage"));
+const DashboardPage = lazy(() => import("./pages/DashboardPage"));
+const PrivateRoute = lazy(() => import("./components/PrivateRoute"));
+const ForgotPasswordPage = lazy(() => import("./pages/ForgotPasswordPage"));
+const UpdateProfilePage = lazy(() => import("./pages/UpdateProfilePage"));
+const TopRatedMovie = lazy(() => import("./pages/topRatedMovie"));
+const SimilarMoviesPage= lazy(() => import("./pages/similarMoviePage"));
+const HomePage = lazy(() => import("./pages/homePage"));
+const MoviePage = lazy(() => import('./pages/movieDetailsPage'));
+const FavoriteMoviesPage = lazy(() => import('./pages/favoritesMoviesPage'));
+const MovieReviewPage = lazy(() => import("./pages/movieReviewPage"));   
 
 
 const App = () => {
@@ -39,6 +39,7 @@ const App = () => {
           <GenresContextProvider>    {/* NEW */}
           <PeopleContextProvider>
           <AuthProvider>
+          <Suspense fallback={<h1>Loading page....</h1>}>
         <Switch>
         <Route exact path="/review/peopleform" component={AddPeopleReviewPage} />
           <Route exact path="/reviews/form" component={AddMovieReviewPage} /> 
@@ -61,6 +62,7 @@ const App = () => {
           <Route path="/" component={HomePage} />
           <Redirect from="*" to="/" />
         </Switch>
+        </Suspense>
         </AuthProvider>
         </PeopleContextProvider>
         </GenresContextProvider>    {/* NEW */}
