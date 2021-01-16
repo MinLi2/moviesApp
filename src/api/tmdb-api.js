640a6ccd2fd51bfac88b4adf148880d88322e3cb
phoenixli-star <phoenixli512@gmail.com>
2021-01-16 09:50:05 +0800

    connect actors in react

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -1,10 +1,20 @@
+// export const getMovies = () => {
+//     return fetch(
+//       `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
+//     )
+//       .then(res => res.json())
+//       .then(json => json.results);
+//   };
 export const getMovies = () => {
-    return fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
-    )
-      .then(res => res.json())
-      .then(json => json.results);
-  };
+  return fetch(
+    '/api/movies',{headers:{
+      'Authorization': window.localStorage.getItem('token') 
+    },
+  method:'get',
+    }
+    ).then(res => res.json());
+    
+};
   
   export const getMovie = id => {
     return fetch(
@@ -46,14 +56,24 @@ export const getMovies = () => {
 
  
 
-  export const getPeople = id => {
-    return fetch(
-      `https://api.themoviedb.org/3/person/popular?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
-    )
-      .then(res => res.json())
-      .then(json => json.results);
-  };
+  // export const getPeople = id => {
+  //   return fetch(
+  //     `https://api.themoviedb.org/3/person/popular?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
+  //   )
+  //     .then(res => res.json())
+  //     .then(json => json.results);
+  // };
 
+  export const getPeople = () => {
+    return fetch(
+      '/api/actors',{headers:{
+        'Authorization': window.localStorage.getItem('token') 
+      },
+    method:'get',
+      }
+      ).then(res => res.json());
+      
+  };
   export const getPeopleDetails = id => {
     return fetch(
       `https://api.themoviedb.org/3/person/${id}?api_key=${process.env.REACT_APP_TMDB_KEY}`
